fa8b3425e0b073553253054c27147f110587e0c9
chen3463 <cqm0802@hotmail.com>
2019-01-23 20:57:26 -0600

    Update BagofWords.py

--- BagofWords.py
+++ BagofWords.py
@@ -1,7 +1,6 @@
 import re
-def extract_words(document, stopwords, stem=None, lax=None):
+def Tokenization(document, stopwords, stem=None, lax=None):
     words = re.sub("[^\w]", " ", document).split()
     words_clean = [words.lower() for w in words if w not in stopwords]
-    
     return words_clean
     
