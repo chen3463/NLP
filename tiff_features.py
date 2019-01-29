3c37e42eb547e95da20dadf2f97c1880666a3365
chen3463 <cqm0802@hotmail.com>
2019-01-28 22:49:06 -0600

    Create tiff_features.py

---
+++ tiff_features.py
@@ -0,0 +1,22 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def tfidf_features(X_train, X_val, X_test):
+    """
+        X_train, X_val, X_test — samples        
+        return TF-IDF vectorized representation of each sample and vocabulary
+    """
+    # Create TF-IDF vectorizer with a proper parameters choice
+    # Fit the vectorizer on the train set
+    # Transform the train, test, and val sets and return the result
+    
+    
+    tfidf_vectorizer = TfidfVectorizer(token_pattern = '(\S+)', ngram_range=(1,2), max_df = 0.9, min_df = 5)####### YOUR CODE HERE #######
+    
+    ######################################
+    ######### YOUR CODE HERE #############
+    ######################################
+    X_train = tfidf_vectorizer.fit_transform(X_train)
+    X_val = tfidf_vectorizer.transform(X_val)
+    X_test = tfidf_vectorizer.transform(X_test)
+    
+    return X_train, X_val, X_test, tfidf_vectorizer.vocabulary_
